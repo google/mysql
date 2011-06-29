3384458a0fdd839a9350f5bac618fd30da27513b
John Gunderman <johngunderman@google.com>
2011-06-29 13:49:09 -0700

    Added var, health, and status support to http server
    The var data can be accessed by navigating to
    
      http://[hostname]:[port]/var
    
    Output of var should mimic the output of executing SHOW STATUS on the
    server. The health and status can be accessed in the same way, but
    replacing /var with /health and /status respectively.
    
    Change-Id: Id633d764f054e1c9e80944808b6d8f2a19cbc5a9

--- sql/httpd_request.cc
+++ sql/httpd_request.cc
@@ -28,6 +28,8 @@
 #include "httpd.h"
 #include "rpl_mi.h"
 
+#define MAX_PREFIX_LENGTH 80                 // somewhat arbitrary
+
 
 bool HTTPRequest::WriteTableHeader(const char *title,
                                    const char *const *headings)
@@ -174,6 +176,515 @@ bool HTTPRequest::GenerateError(const char *msg)
 {
   WriteBody(msg);
   return false;
+}
+
+/**
+   Output a list of status variables that would otherwise be achieved
+   by commands like 'SHOW VARIABLES' and 'SHOW STATUS'.
+
+   Calling function must hold the lock on LOCK_status.
+
+   @param[in]  prefix   prefix for the variables in #vars
+   @param[in]  vars     an array of SHOW_VARs to be printed
+
+   @return Operation status
+     @retval  0       OK
+     @retval  errno   ERROR
+*/
+
+int HTTPRequest::var_GenVars(const char *prefix, SHOW_VAR *vars)
+{
+  bool error= false;
+  STATUS_VAR tmp_stat;
+  SHOW_VAR *var;
+  my_aligned_storage<SHOW_VAR_FUNC_BUFF_SIZE, MY_ALIGNOF(long)> buffer;
+  char * const buff= buffer.data;
+
+  calc_sum_of_all_status(&tmp_stat);
+
+  if (prefix == NULL)
+    prefix= "";
+
+  for (SHOW_VAR *variables= vars; variables->name && !error; variables++)
+  {
+    char *result, *resultend;
+    char varbuf[1024];
+    SHOW_VAR tmp;
+
+    /*
+      if var->type is SHOW_FUNC, call the function.
+      Repeat as necessary, if new var is again SHOW_FUNC
+    */
+    for (var=variables; var->type == SHOW_FUNC; var= &tmp)
+      ((mysql_show_var_func)(var->value))(thd_, &tmp, buff);
+    tmp= *var;
+
+    if (tmp.type == SHOW_ARRAY)
+    {
+      char new_prefix[MAX_PREFIX_LENGTH];
+      // +2, one for the '_' and the other for the \0
+      char name[MAX_PREFIX_LENGTH * 2 + 2];
+
+      // lowercase our prefix. Mostly useful for Com_ variables.
+      strncpy(name, tmp.name, MAX_PREFIX_LENGTH);
+      name[0]= (char) tolower((int) name[0]);
+
+      strncpy(new_prefix, prefix, MAX_PREFIX_LENGTH);
+      strncat(new_prefix, name, MAX_PREFIX_LENGTH);
+      strncat(new_prefix, "_", MAX_PREFIX_LENGTH);
+      var_GenVars(new_prefix, (SHOW_VAR *) tmp.value);
+    }
+    else
+      get_variable_value(thd_, &tmp, OPT_GLOBAL, &tmp_stat,
+                         varbuf, &result, &resultend);
+
+    /*
+      MySQL status variables are always capitalised on the first
+      character, so we choose to lowercase it here to fit in with
+      the existing format provided by the mysql_var_reporter.
+    */
+    error |= WriteBodyFmt("%s%c%s %s\r\n",
+                          prefix, tolower(variables->name[0]),
+                          variables->name + 1, result);
+  }
+  return (error) ? ER_OUT_OF_RESOURCES : 0;
+}
+
+/**
+  Output the contents of SHOW STATUS to the http response.
+
+  Handles locking of the status variables that is necessary
+  for ::var_GenVars to function properly.
+
+  @return  Operation Status
+    @retval  0      OK
+    @retval  errno  ERROR
+*/
+
+int HTTPRequest::var_ShowStatus()
+{
+  pthread_mutex_lock(&LOCK_status);
+  /*
+    The mysql_var_reporter was responsible for prepending
+    variable names with 'status_'.
+  */
+  int ret= var_GenVars("status_", status_vars);
+  pthread_mutex_unlock(&LOCK_status);
+  return ret;
+}
+
+
+/**
+   Output the contents of  "SHOW MASTER STATUS" to the http repsonse.
+
+   @return  Operation Status
+     @retval  0      OK
+     @retval  errno  ERROR
+ */
+
+int HTTPRequest::var_MasterStatus()
+{
+  Master_info *mi= active_mi;
+  bool err= false;
+  ulonglong group_id;
+  uint32 group_server_id;
+
+  if (mi == NULL || mi->host[0] == '\0')
+  {
+    err|= WriteBodyFmt("master_configured 1\r\n");
+    err|= WriteBodyFmt("slave_configured 0\r\n");
+    return (err) ? ER_OUT_OF_RESOURCES : 0;
+  }
+
+  mysql_bin_log.get_group_and_server_id(&group_id, &group_server_id);
+
+  err|= WriteBodyFmt("master_configured 0\r\n");
+  err|= WriteBodyFmt("slave_configured 1\r\n");
+
+  pthread_mutex_lock(&mi->data_lock);
+  pthread_mutex_lock(&mi->rli.data_lock);
+  in_addr ip;
+  inet_pton(AF_INET, mi->host, &ip);
+  err|= WriteBodyFmt("master_host %d\r\n", (uint32) ip.s_addr);
+  err|= WriteBodyFmt("master_port %d\r\n", (uint32) mi->port);
+  err|= WriteBodyFmt("connect_retry %d\r\n", (uint32) mi->connect_retry);
+  err|= WriteBodyFmt("read_master_log_pos %lld\r\n", mi->master_log_pos);
+  err|= WriteBodyFmt("read_master_log_name %s\r\n", mi->master_log_name);
+  err|= WriteBodyFmt("relay_log_pos %lld\r\n", mi->rli.group_relay_log_pos);
+  err|= WriteBodyFmt("relay_log_name %s\r\n", mi->rli.group_relay_log_name);
+  err|= WriteBodyFmt("slave_io_running %d\r\n",
+                     (mi->slave_running == MYSQL_SLAVE_RUN_CONNECT) ? 1 : 0);
+  err|= WriteBodyFmt("slave_sql_running %d\r\n",
+                     mi->rli.slave_running ? 1 : 0);
+  pthread_mutex_lock(&mi->err_lock);
+  pthread_mutex_lock(&mi->rli.err_lock);
+  err |= WriteBodyFmt("last_sql_errno %d\r\n", (uint32) mi->rli.last_error().number);
+  err |= WriteBodyFmt("last_io_errno %d\r\n", (uint32) mi->last_error().number);
+  pthread_mutex_unlock(&mi->rli.err_lock);
+  pthread_mutex_unlock(&mi->err_lock);
+  err|= WriteBodyFmt("skip_counter %d\r\n",
+                     (uint32) mi->rli.slave_skip_counter);
+  err|= WriteBodyFmt("exec_master_log_pos %d\r\n",
+                     (uint32) mi->rli.group_master_log_pos);
+  err|= WriteBodyFmt("exec_master_log_name %s\r\n",
+                     mi->rli.group_master_log_name);
+  err|= WriteBodyFmt("relay_log_space %d\r\n",
+                     (uint32) mi->rli.log_space_total);
+  err|= WriteBodyFmt("until_log_pos %d\r\n",
+                     (uint32) mi->rli.until_log_pos);
+  err|= WriteBodyFmt("until_log_name %s\r\n",
+                     mi->rli.until_log_name);
+  err|= WriteBodyFmt("master_ssl_allowed %d\r\n",
+                     (uint32) mi->ssl ? 1 : 0);
+  err|= WriteBodyFmt("group_id %d\r\n",
+                     (uint32) group_id);
+  err|= WriteBodyFmt("group_server_id %d\r\n",
+                     (uint32) group_server_id);
+
+  long secs= 0;
+
+  if (mi->slave_running == MYSQL_SLAVE_RUN_CONNECT && mi->rli.slave_running)
+  {
+    secs= (long)((time_t)time(NULL) - mi->rli.last_master_timestamp)
+      - mi->clock_diff_with_master;
+
+    if (secs < 0 || mi->rli.last_master_timestamp == 0)
+      secs= 0;
+  }
+
+  err|= WriteBodyFmt("seconds_behind_master %lld\r\n", secs);
+  pthread_mutex_unlock(&mi->rli.data_lock);
+  pthread_mutex_unlock(&mi->data_lock);
+  return (err) ? ER_OUT_OF_RESOURCES : 0;
+}
+
+
+/**
+   Output the MySQL process table to the http response.
+
+   @return  Operation Status
+     @retval  0      OK
+     @retval  errno  ERROR
+ */
+
+int HTTPRequest::var_ProcessList()
+{
+  int num_procs= 0;
+  int idle_procs= 0;
+  int active_procs= 0;
+  long long int total_proc_time= 0;
+  time_t now= time(NULL);
+  time_t oldest_start_time= now;
+
+  pthread_mutex_lock(&LOCK_thread_count);
+  I_List_iterator<THD> it(threads);
+  THD *tmp;
+  while ((tmp= it++))
+  {
+    num_procs++;
+    if (tmp->locked || tmp->net.reading_or_writing)
+    {
+      if (tmp->start_time < oldest_start_time)
+        oldest_start_time= tmp->start_time;
+      active_procs++;
+    }
+    else
+    {
+      idle_procs++;
+    }
+    total_proc_time+= now - tmp->start_time;
+  }
+  pthread_mutex_unlock(&LOCK_thread_count);
+
+  long long int mean_proc_time= 0;
+  if (num_procs > 0)
+    mean_proc_time= total_proc_time / num_procs;
+  bool err= false;
+  err|= WriteBodyFmt("num_procs %d\r\n", num_procs);
+  err|= WriteBodyFmt("idle_procs %d\r\n", idle_procs);
+  err|= WriteBodyFmt("active_procs %d\r\n", active_procs);
+  err|= WriteBodyFmt("mean_proc_time %lld\r\n", mean_proc_time);
+  err|= WriteBodyFmt("total_proc_time %lld\r\n", total_proc_time);
+  err|= WriteBodyFmt("oldest_active_proc_time %d\r\n", oldest_start_time);
+  return (err) ? ER_OUT_OF_RESOURCES : 0;
+}
+
+/**
+  Generate a process listing.
+*/
+int HTTPRequest::status_ProcessListing(time_t current_time)
+{
+  const char *headings[]= { "Id", "User", "Host", "Database", "Command",
+                            "Time", "State", "Info", NULL };
+  WriteTableHeader("Thread List", headings);
+  pthread_mutex_lock(&LOCK_thread_count);
+  I_List_iterator<THD> it(threads);
+  THD *tmp;
+  while ((tmp= it++))
+  {
+    Security_context *tmp_sctx= tmp->security_ctx;
+    if (tmp->vio_ok() || tmp->system_thread)
+    {
+      WriteBodyFmt(" <tr>\r\n");
+      WriteTableColumn(tmp->thread_id);
+      WriteTableColumn((tmp_sctx->user) ? tmp_sctx->user :
+                       (tmp->system_thread ?
+                        "system user" : "unauthenticated user"));
+
+      // Column Host
+      if (tmp->peer_port && (tmp_sctx->host || tmp_sctx->ip)
+          && thd_->security_ctx->host_or_ip[0])
+        WriteTableColumn(tmp_sctx->host_or_ip, tmp->peer_port);
+      else
+        WriteTableColumn((tmp_sctx->host_or_ip[0]) ? tmp_sctx->host_or_ip
+                         : (tmp_sctx->host) ? tmp_sctx->host : "");
+
+      // Columns Database and Command
+      WriteTableColumn((tmp->db) ? tmp->db : "");
+      WriteTableColumn(command_name[tmp->command].str);
+
+      // Column Time
+      WriteTableColumn(current_time - tmp->start_time);
+
+      // Column State
+      if (tmp->killed == THD::KILL_CONNECTION)
+        WriteTableColumn("Killed");
+      else
+      {
+        if (tmp->mysys_var)
+          pthread_mutex_lock(&tmp->mysys_var->mutex);
+        WriteTableColumn((tmp->locked ? "Locked" :
+                          tmp->net.reading_or_writing ?
+                          (tmp->net.reading_or_writing == 2 ?
+                           "Writing to net" :
+                           tmp->command == COM_SLEEP ? ""
+                           : "Reading from net") :
+                          tmp->proc_info ? tmp->proc_info :
+                          tmp->mysys_var &&
+                          tmp->mysys_var->current_cond
+                          ? "Waiting on cond" : ""));
+        if (tmp->mysys_var)
+          pthread_mutex_unlock(&tmp->mysys_var->mutex);
+      }
+
+      // Column Query
+      WriteTableColumn((tmp->query()) ? tmp->query() : "");
+      WriteBodyFmt(" </tr>\r\n");
+    }
+  }
+  pthread_mutex_unlock(&LOCK_thread_count);
+  WriteBodyFmt(" </table>\r\n");
+  return 0;
+}
+
+/**
+  Generate Master status
+*/
+int HTTPRequest::status_MasterStatus()
+{
+  Master_info *mi= active_mi;
+  // Check if we are a slave server
+  if (mi != NULL && mi->host[0] != '\0')
+    return 0;
+
+  if (mysql_bin_log.is_open())
+  {
+    const char *headings[] = { "File",
+                               "Position",
+                               "Group ID / Server ID",
+                               NULL };
+    WriteTableHeader("Master Status", headings);
+    WriteTableRowStart();
+    // Master server
+    LOG_INFO li;
+    mysql_bin_log.get_current_log(&li);
+    WriteTableColumn(li.log_file_name);
+    WriteTableColumn(li.pos);
+    WriteBodyFmt("  <td>%lld / %lld</td>\r\n",
+                 li.group_id, li.server_id);
+    WriteTableRowEnd();
+    WriteTableEnd();
+  }
+  return 0;
+}
+
+/**
+   Prints SHOW SLAVE STATUS information to the http response.
+
+   @return Operation Status
+     @retval  0   OK
+*/
+
+int HTTPRequest::status_SlaveStatus()
+{
+  Master_info *mi = active_mi;
+  ulonglong group_id;
+  uint32 server_id;
+  // Check if we are a master server.
+  if (mi == NULL || mi->host[0] == '\0')
+    return 0;
+  const char *headings[] = { "Master Host", "User", "Connect Retries",
+                             "Master Log File / Position",
+                             "Relay Log File / Position",
+                             "Relay Master Log File / Position",
+                             "Group ID / Server ID",
+                             "Slave IO / SQL",
+                             "Seconds Behind",
+                             NULL };
+  WriteTableHeader("Slave Status", headings);
+  WriteTableRowStart();
+  mysql_bin_log.get_group_and_server_id(&group_id, &server_id);
+  // Slave server
+  pthread_mutex_lock(&mi->data_lock);
+  pthread_mutex_lock(&mi->rli.data_lock);
+  WriteTableColumn(mi->host, mi->port);
+  WriteTableColumn(mi->user);
+  WriteTableColumn((unsigned long) mi->connect_retry);
+  WriteBodyFmt("  <td>%s / %lld</td>\r\n",
+               (mi->master_log_name[0] == '\0')
+               ? "&lt;empty>" : mi->master_log_name,
+               mi->master_log_pos);
+  WriteBodyFmt("  <td>%s / %lld</td>\r\n",
+               (mi->rli.group_relay_log_name[0] == '\0')
+               ? "&lt;empty>" : mi->rli.group_relay_log_name,
+               mi->rli.group_relay_log_pos);
+  WriteBodyFmt("  <td>%s / %lld</td>\r\n",
+               (mi->rli.group_master_log_name[0] == '\0')
+               ? "&lt;empty>" : mi->rli.group_master_log_name,
+               mi->rli.group_master_log_pos);
+  WriteBodyFmt("  <td>%lld / %lld</td>\r\n",
+               group_id, server_id);
+
+  // Columns Slave IO and Slave SQL
+  WriteBodyFmt("  <td>%s / %s</td>\r\n",
+               (mi->slave_running == MYSQL_SLAVE_RUN_CONNECT) ? "Yes" : "No",
+               (mi->rli.slave_running) ? "Yes" : "No");
+  long secs= 0;
+  if (mi->slave_running == MYSQL_SLAVE_RUN_CONNECT && mi->rli.slave_running)
+  {
+    secs= (long)((time_t)time(NULL) - mi->rli.last_master_timestamp)
+      - mi->clock_diff_with_master;
+    if (secs < 0 || mi->rli.last_master_timestamp == 0)
+      secs= 0;
+  }
+  // Column Seconds Behind
+  WriteTableColumn(secs);
+
+  pthread_mutex_unlock(&mi->rli.data_lock);
+  pthread_mutex_unlock(&mi->data_lock);
+
+  WriteTableRowEnd();
+  WriteTableEnd();
+  return 0;
+}
+
+/**
+   Generate a response body for a /status URL.
+
+   @return Operation Status
+     @retval  0     OK
+     @retval  errno ERROR
+*/
+
+int HTTPRequest::status(void)
+{
+  time_t current_time= time(0);
+
+  WriteBody("<html><head>\r\n"
+            "<meta HTTP-EQUIV=\"content-type\" "
+            "CONTENT=\"text/html; charset=UTF-8\">\r\n");
+  WriteBody("<title>Status for MySQL</title>\r\n</head>\r\n");
+  WriteBody("<body bgcolor=#ffffff text=#000000>\r\n");
+  WriteBodyFmt("<p><table bgcolor=#eeeeff width=100%>"
+               "<tr align=center><td>"
+               "<font size=+2>Status for MySQL %s %s</font>"
+               "</td></tr></table></p>\r\n", server_version,
+               MYSQL_COMPILATION_COMMENT);
+
+  WriteBody("<table cellspacing=0 cellpadding=0 width=100%><tr>\r\n");
+  {
+    time_t diff= current_time - server_start_time;
+    char start_time_str[64];
+    ctime_r(&server_start_time, start_time_str);
+    WriteBodyFmt("<td>Started: %s -- up %lld seconds<br>\r\n", start_time_str,
+                 diff);
+  }
+  WriteBody("<br></td>\r\n");
+
+  WriteBody("<td align=right valign=top>\r\n");
+  WriteBodyFmt("Running on %s<br>\r\n", glob_hostname);
+  WriteBody("</td></tr></table>\r\n");
+  WriteBody("<p><a href=\"/var\">Local variables</a></p>");
+
+  pthread_mutex_lock(&LOCK_server_started);
+  if (mysqld_server_started)
+  {
+    pthread_mutex_unlock(&LOCK_server_started);
+    status_ProcessListing(current_time);
+    status_MasterStatus();
+    status_SlaveStatus();
+    // TODO: status_UserStats();
+    // TODO: status_TableStats();
+  }
+  else
+  {
+    pthread_mutex_unlock(&LOCK_server_started);
+    WriteBody("<p><font size=+2>MySQL is currently initialising</font></p>");
+  }
+  WriteBody("</body></html>");
+  return 0;
+}
+
+/**
+  Generate a response body for a /var URL.
+
+  @return Operation Status
+    @retval  0     OK
+    @retval  errno ERROR
+*/
+
+int HTTPRequest::var(void)
+{
+  /*
+    Test that the server is fully initialised.  Most of these
+    statistics will fail until the table handlers have been
+    initialised.
+  */
+  pthread_mutex_lock(&LOCK_server_started);
+  if (!mysqld_server_started)
+  {
+    pthread_mutex_unlock(&LOCK_server_started);
+    return 0;
+  }
+  pthread_mutex_unlock(&LOCK_server_started);
+
+  int err= var_ShowStatus();
+
+  //TODO: var_ShowwInnoDBStatus();
+  //TODO: var_UserStats();
+  //TODO: var_TableStats();
+
+  if (!err)
+    err= var_MasterStatus();
+  if (!err)
+    err= var_ProcessList();
+
+  return err;
+}
+
+/**
+  Generate a response body for a /health URL.
+
+
+  @return Operation Status
+    @retval  0     OK
+    @retval  errno ERROR
+*/
+
+int HTTPRequest::health(void)
+{
+  WriteBody(STRING_WITH_LEN("OK\r\n"));
+  return 0;
 }
 
 /**
