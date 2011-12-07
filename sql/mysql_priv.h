3bb0f38b7a595ceb503a2085263f912dfd046222
Justin Tolmer <jtolmer@google.com>
2011-12-06 16:03:46 -0800

    Add options to disable questionable features
    In our 5.0 tree we disabled all these features with a single
    flag. This patch separates them out into separate flags so
    that they can be disabled separately.
    
    Change-Id: I3e1059e48bc68a065acfa4faa23c2c88de33efcb

--- sql/mysql_priv.h
+++ sql/mysql_priv.h
@@ -2017,6 +2017,11 @@ extern uint opt_large_page_size;
 extern char *opt_logname, *opt_slow_logname;
 extern const char *log_output_str;
 
+extern my_bool opt_allow_views;
+extern my_bool opt_allow_triggers;
+extern my_bool opt_allow_stored_procedures;
+extern my_bool opt_allow_subqueries;
+
 extern MYSQL_PLUGIN_IMPORT MYSQL_BIN_LOG mysql_bin_log;
 extern LOGGER logger;
 extern TABLE_LIST general_log, slow_log;
