7c2727cd1f6b2a37036605acbf2352013455dfe3
Justin Tolmer <jtolmer@google.com>
2011-12-13 12:55:08 -0800

    Cleanup of the Google allow_* options
    Some minor cleanup of the allow_* options we've added; alphabetizing
    them and hardening the test a little bit.
    
    Change-Id: If84d43fd69d6b9487cc18952ec85016aa36bb7b5

--- sql/set_var.cc
+++ sql/set_var.cc
@@ -167,17 +167,16 @@ static sys_var_chain vars = { NULL, NULL };
 
 static sys_var_bool_ptr sys_allow_delayed_write(&vars, "allow_delayed_write",
                                                 &opt_allow_delayed_write);
-static sys_var_bool_ptr sys_allow_views(&vars, "allow_views",
-                                        &opt_allow_views);
-static sys_var_bool_ptr sys_allow_triggers(&vars, "allow_triggers",
-                                           &opt_allow_triggers);
 static sys_var_bool_ptr
   sys_allow_stored_procedures(&vars,
                               "allow_stored_procedures",
                               &opt_allow_stored_procedures);
 static sys_var_bool_ptr sys_allow_subqueries(&vars, "allow_subqueries",
                                              &opt_allow_subqueries);
-
+static sys_var_bool_ptr sys_allow_triggers(&vars, "allow_triggers",
+                                           &opt_allow_triggers);
+static sys_var_bool_ptr sys_allow_views(&vars, "allow_views",
+                                        &opt_allow_views);
 static sys_var_thd_ulong
 sys_auto_increment_increment(&vars, "auto_increment_increment",
                              &SV::auto_increment_increment, NULL, NULL,
