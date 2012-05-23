faece3e5dbacce018dd85b8ec38c2b24cff3ec13
John Gunderman <johngunderman@google.com>
2012-05-23 16:02:54 -0700

    Remove while loop which appears to only add unnecessary scoping
    Change-Id: I65776fd65660bce2330efbd0ac4d38e19fd4283c
    Reviewed-by: Sean Rees <seanrees@google.com>

--- sql/httpd.cc
+++ sql/httpd.cc
@@ -562,37 +562,34 @@ pthread_handler_t httpd_handle_connection(void *arg)
   }
 
 
-  do
-  {
-    /* RESTRICTED SCOPE */ {
-      Http_request req(thd);
-      NET *net= &thd->net;
+  /* RESTRICTED SCOPE */ {
+    Http_request req(thd);
+    NET *net= &thd->net;
 
-      /* Use "connect_timeout" value during connection phase. */
-      my_net_set_read_timeout(net, connect_timeout);
-      my_net_set_write_timeout(net, connect_timeout);
+    /* Use "connect_timeout" value during connection phase. */
+    my_net_set_read_timeout(net, connect_timeout);
+    my_net_set_write_timeout(net, connect_timeout);
 
-      thd_proc_info(thd, 0);
-      thd->command= COM_SLEEP;
-      thd->set_time();
+    thd_proc_info(thd, 0);
+    thd->command= COM_SLEEP;
+    thd->set_time();
 
-      /* Connect completed, set read/write timeouts back to tdefault. */
-      my_net_set_read_timeout(net, thd->variables.net_read_timeout);
-      my_net_set_write_timeout(net, thd->variables.net_write_timeout);
+    /* Connect completed, set read/write timeouts back to tdefault. */
+    my_net_set_read_timeout(net, thd->variables.net_read_timeout);
+    my_net_set_write_timeout(net, thd->variables.net_write_timeout);
 
-      httpd_read_request(thd);
-      int error= httpd_process_request(thd, &req);
-      if (! error)
-        httpd_send_response(thd, &req);
-      httpd_close_connection(thd, error, 1);
-    }
+    httpd_read_request(thd);
+    int error= httpd_process_request(thd, &req);
+    if (! error)
+      httpd_send_response(thd, &req);
+    httpd_close_connection(thd, error, 1);
+  }
 
-    /* This call to end_thread should never return.  */
-    httpd_end_thread(thd);
-    /* If end_thread returns, we are either running with --one-thread. */
-    thd= current_thd;
-    thd->thread_stack= (char*) &thd;
-  } while (true);
+  /* This call to end_thread should never return.  */
+  httpd_end_thread(thd);
+  /* If end_thread returns, we are either running with --one-thread. */
+  thd= current_thd;
+  thd->thread_stack= (char*) &thd;
 
   return 0;
 }
