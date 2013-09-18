0c7b58f758c01b51280043c16701e1091f641dc9
Jeremy Cole <jeremycole@google.com>
2013-09-18 14:22:02 -0700

    Fix user and hostname used by automatic_sp_privileges
    When a user without global EXECUTE privileges creates a stored procedure
    with automatic_sp_privileges mode enabled, a procedure-specific EXECUTE
    privilege would be automatically granted on that user. In the function
    which handles this automatic grant, the user and host used for the grant
    were derived from the actual connected user and resolved hostname, not
    on the stored ACL under which the user connected.
    
    If a user was connected using a "fuzzy" ACL, such as a host containing a
    wildcard, this could cause a completely new user (with the exact host of
    the connected user) to be auto-created. In practice this would mean the
    automatic privileges granted could actually cause the user in question
    to *lose* privileges (having only USAGE) when connecting from the same
    specific host, on the global context, while gaining the intended
    procedure-specific EXECUTE privilege.
    
    The correct behavior is always to add the automatic grant on the user
    as-authenticated, which is stored in the Security_context as priv_user
    and priv_host.
    
    Additionally, a test for MySQL Bug 44658 was removed from grant.test
    because it was testing behavior that only existed due to this bug.
    
    Change-Id: I14a32a4392d4e1a8bad38276f17a071280fc1b09

--- sql/sql_acl.cc
+++ sql/sql_acl.cc
@@ -10089,32 +10089,11 @@ bool sp_grant_privileges(THD *thd, const char *sp_db, const char *sp_name,
   TABLE_LIST tables[1];
   List<LEX_USER> user_list;
   bool result;
-  ACL_USER *au;
-  char passwd_buff[SCRAMBLED_PASSWORD_CHAR_LENGTH+1];
   Dummy_error_handler error_handler;
   DBUG_ENTER("sp_grant_privileges");
 
   if (!(combo=(LEX_USER*) thd->alloc(sizeof(st_lex_user))))
     DBUG_RETURN(TRUE);
-
-  combo->user.str= sctx->user;
-
-  mysql_mutex_lock(&acl_cache->lock);
-
-  if ((au= find_user_wild(combo->host.str=(char*)sctx->host_or_ip, combo->user.str)))
-    goto found_acl;
-  if ((au= find_user_wild(combo->host.str=(char*)sctx->host, combo->user.str)))
-    goto found_acl;
-  if ((au= find_user_wild(combo->host.str=(char*)sctx->ip, combo->user.str)))
-    goto found_acl;
-  if ((au= find_user_wild(combo->host.str=(char*)"%", combo->user.str)))
-    goto found_acl;
-
-  mysql_mutex_unlock(&acl_cache->lock);
-  DBUG_RETURN(TRUE);
-
- found_acl:
-  mysql_mutex_unlock(&acl_cache->lock);
 
   bzero((char*)tables, sizeof(TABLE_LIST));
   user_list.empty();
@@ -10122,43 +10101,12 @@ bool sp_grant_privileges(THD *thd, const char *sp_db, const char *sp_name,
   tables->db= (char*)sp_db;
   tables->table_name= tables->alias= (char*)sp_name;
 
-  thd->make_lex_string(&combo->user, combo->user.str, strlen(combo->user.str));
-  thd->make_lex_string(&combo->host, combo->host.str, strlen(combo->host.str));
+  thd->make_lex_string(&combo->user, sctx->priv_user, strlen(sctx->priv_user));
+  thd->make_lex_string(&combo->host, sctx->priv_host, strlen(sctx->priv_host));
 
   combo->password= empty_lex_str;
   combo->plugin= empty_lex_str;
   combo->auth= empty_lex_str;
-
-  if(au)
-  {
-    if (au->salt_len)
-    {
-      if (au->salt_len == SCRAMBLE_LENGTH)
-      {
-        make_password_from_salt(passwd_buff, au->salt);
-        combo->password.length= SCRAMBLED_PASSWORD_CHAR_LENGTH;
-      }
-      else if (au->salt_len == SCRAMBLE_LENGTH_323)
-      {
-        make_password_from_salt_323(passwd_buff, (ulong *) au->salt);
-        combo->password.length= SCRAMBLED_PASSWORD_CHAR_LENGTH_323;
-      }
-      else
-      {
-        push_warning_printf(thd, Sql_condition::WARN_LEVEL_WARN, ER_PASSWD_LENGTH,
-                            ER(ER_PASSWD_LENGTH), SCRAMBLED_PASSWORD_CHAR_LENGTH);
-        return TRUE;
-      }
-      combo->password.str= passwd_buff;
-    }
-
-    if (au->plugin.str != native_password_plugin_name.str &&
-        au->plugin.str != old_password_plugin_name.str)
-    {
-      combo->plugin= au->plugin;
-      combo->auth= au->auth_string;
-    }
-  }
 
   if (user_list.push_back(combo))
     DBUG_RETURN(TRUE);
